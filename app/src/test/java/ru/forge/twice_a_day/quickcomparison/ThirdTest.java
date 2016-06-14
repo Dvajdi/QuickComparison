2babcff134ee7d1bebb6b9f4d1fed10199d9cb46
twice <dvajdi@bk.ru>
2016-06-14 15:41:54 +0300

    добавлен тест

---
+++ app/src/test/java/ru/forge/twice_a_day/quickcomparison/ThirdTest.java
@@ -0,0 +1,22 @@
+package ru.forge.twice_a_day.quickcomparison;
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import ru.forge.twice_a_day.quickcomparison.about_units.Unit;
+
+public class ThirdTest {
+    @Test
+    public void razn(){
+        Unit u = new Unit("unit","u",1-0.009,true,false);
+        if((u.getValue()<0.001)&(u.getValue()>0)){
+            Assert.assertTrue("pupsiki",true);}else{ Assert.assertTrue(false);}
+    }
+
+    @Test
+    public void proba(){
+        Unit u = new Unit("unit","u",1-0.009,true,false);
+        if((u.getValue()<0.001)&(u.getValue()>0)){
+            Assert.assertFalse("pupsiki",true);}else{ Assert.assertFalse(false);}
+    }
+}
